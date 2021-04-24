672e31aaa58d99324a85b665e6b0470c9e962fd7
NyGmember <nygmember.002@gmail.com>
2021-04-24 22:52:30 +0700

    portfolio to angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRouteModule } from "./app-route.module";
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { ContactusComponent } from './contactus/contactus.component';
-import { ImageProfileComponent } from './contactus/image-profile/image-profile.component';
-import { TextItemComponent } from './portItem/text-item/text-item.component';
-import { LevelItemComponent } from './portItem/level-item/level-item.component';
-import { HeaderComponent } from './home/header/header.component';
+import { ImageProfileComponent } from './image-profile/image-profile.component';
+import { HeaderComponent } from "./header/header.component";
+import { QouteComponent } from './qoute/qoute.component';
+import { PortfoiloComponent } from './portfoilo/portfoilo.component';
+import { LevelItemComponent } from "./level-item/level-item.component";
 
 @NgModule({
   declarations: [
@@ -16,13 +20,17 @@ import { HeaderComponent } from './home/header/header.component';
     HomeComponent,
     ContactusComponent,
     ImageProfileComponent,
-    TextItemComponent,
-    LevelItemComponent,
-    HeaderComponent
+    HeaderComponent,
+    QouteComponent,
+    PortfoiloComponent,
+    LevelItemComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRouteModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
